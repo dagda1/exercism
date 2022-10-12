af477887e2dfda10db3d9b0c52901f055d78a80d
Paul Cowan <paul.cowan@cutting.scot>
2022-10-12 10:35:59 +0100

    hello world rust

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -7,12 +7,12 @@ isPrime n
   | n `mod` 2 == 0 = False
   | otherwise = not $ any (\x -> n `mod` x == 0) $ tail [1, 3..(ceiling . sqrt . fromIntegral) n]
 
-nthPrime :: Int -> Int -> Int -> Int -> Int
+nthPrime :: Int -> Int -> Int -> Int -> Maybe Integer
 nthPrime n count currentCount currentPrime
-  | n == 1                  = 2
-  | count > 110000          = -1
-  | currentCount > n        = -1
-  | currentCount == n       = currentPrime
+  | n == 1                  = Just $ fromIntegral 2
+  | count > 110000          = Nothing
+  | currentCount > n        = Nothing
+  | currentCount == n       = Just $ fromIntegral currentPrime
   | otherwise               = if (isPrime count) == True then 
                                 nthPrime n (count + 1) (currentCount + 1) count
                               else
@@ -20,5 +20,5 @@ nthPrime n count currentCount currentPrime
 
 
                           
-nth :: Int -> Int
+nth :: Int -> Maybe Integer
 nth n = nthPrime n 3 1 2
