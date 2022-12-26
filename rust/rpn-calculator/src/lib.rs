94121f7095d76d6d03243d83164ab8be8f01726b
Paul Cowan <paul.cowan@cutting.scot>
2022-12-26 17:55:13 +0000

    refactor to option

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -4,64 +4,41 @@ pub enum CalculatorInput {
     Subtract,
     Multiply,
     Divide,
-    Value(i16),
+    Value(i32),
 }
 
-pub enum Operation {
-    Add,
-    Sub,
-    Div,
-    Mul
-}
-
-pub fn unwrap(stack: &mut Vec<i16>, op: Operation, current: Option<i16>) -> i16 {
-    let mut sub_total = if current.is_none() { 0 } else { current.unwrap()};
-                
-    while let Some(next) = stack.pop() {
-        match op {
-            Operation::Add => sub_total += next,
-            Operation::Sub => sub_total -= next,
-            Operation::Mul => sub_total *= next,
-            Operation::Div => sub_total /= next
-        }
-    }
-
-    match current {
-        Some(p) => p + &sub_total,
-        None => sub_total
-    }
-}
-
-pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i16> {
+pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     if inputs.is_empty() {
         return None;
     }
 
-    let mut total: Option<i16>= None;
-    let mut stack: Vec<i16> = Vec::new();
+    let mut stack: Vec<i32> = Vec::new();
 
-
-    for i in inputs.iter() {
-        match i {
+    for input in inputs {
+        match input {
             CalculatorInput::Value(v) => stack.push(*v),
-            CalculatorInput::Add => total = Some(unwrap(&mut stack, Operation::Add, total)),
-            CalculatorInput::Subtract => println!("sub"),
-            CalculatorInput::Multiply => println!("mul"),
-            CalculatorInput::Divide => println!("div"),
+            _ => {
+                if stack.len() < 2 {
+                    return None;
+                }
+
+                let right = stack.pop().unwrap();
+                let left = stack.pop().unwrap();
+
+                match input {
+                    CalculatorInput::Add => stack.push(left + right),
+                    CalculatorInput::Subtract => stack.push(left - right),
+                    CalculatorInput::Multiply => stack.push(left * right),
+                    CalculatorInput::Divide => stack.push(left / right),
+                    _ => return None,
+                }
+            }
         };
-
     }
 
-    println!("-------------------");
-    println!("{:?}", stack);
-    println!("{:?}", total);
-    println!("-------------------");
-
-    match stack.len() {
-        1 => match total {
-            Some(_) => total,
-            None => Some(stack[0])
-        },
-        _ => total
+    if stack.len() != 1 {
+        return None;
     }
+
+    stack.pop()
 }
--- rust/rpn-calculator/tests/rpn-calculator.rs
+++ rust/rpn-calculator/tests/rpn-calculator.rs
@@ -32,58 +32,58 @@ fn test_simple_addition() {
     assert_eq!(evaluate(&input), Some(4));
 }
 
-// #[test]
-// // #[ignore]
-// fn test_simple_subtraction() {
-//     let input = calculator_input("7 11 -");
-//     assert_eq!(evaluate(&input), Some(-4));
-// }
-
-// #[test]
-// // #[ignore]
-// fn test_simple_multiplication() {
-//     let input = calculator_input("6 9 *");
-//     assert_eq!(evaluate(&input), Some(54));
-// }
-
-// #[test]
-// // #[ignore]
-// fn test_simple_division() {
-//     let input = calculator_input("57 19 /");
-//     assert_eq!(evaluate(&input), Some(3));
-// }
-
-// #[test]
-// // #[ignore]
-// fn test_complex_operation() {
-//     let input = calculator_input("4 8 + 7 5 - /");
-//     assert_eq!(evaluate(&input), Some(6));
-// }
-
-// #[test]
+#[test]
 // #[ignore]
-// fn test_too_few_operands_returns_none() {
-//     let input = calculator_input("2 +");
-//     assert_eq!(evaluate(&input), None);
-// }
+fn test_simple_subtraction() {
+    let input = calculator_input("7 11 -");
+    assert_eq!(evaluate(&input), Some(-4));
+}
 
-// #[test]
+#[test]
 // #[ignore]
-// fn test_too_many_operands_returns_none() {
-//     let input = calculator_input("2 2");
-//     assert_eq!(evaluate(&input), None);
-// }
+fn test_simple_multiplication() {
+    let input = calculator_input("6 9 *");
+    assert_eq!(evaluate(&input), Some(54));
+}
 
-// #[test]
-// #[ignore]
-// fn test_zero_operands_returns_none() {
-//     let input = calculator_input("+");
-//     assert_eq!(evaluate(&input), None);
-// }
+#[test]
+#[ignore]
+fn test_simple_division() {
+    let input = calculator_input("57 19 /");
+    assert_eq!(evaluate(&input), Some(3));
+}
 
-// #[test]
-// #[ignore]
-// fn test_intermediate_error_returns_none() {
-//     let input = calculator_input("+ 2 2 *");
-//     assert_eq!(evaluate(&input), None);
-// }
+#[test]
+#[ignore]
+fn test_complex_operation() {
+    let input = calculator_input("4 8 + 7 5 - /");
+    assert_eq!(evaluate(&input), Some(6));
+}
+
+#[test]
+#[ignore]
+fn test_too_few_operands_returns_none() {
+    let input = calculator_input("2 +");
+    assert_eq!(evaluate(&input), None);
+}
+
+#[test]
+#[ignore]
+fn test_too_many_operands_returns_none() {
+    let input = calculator_input("2 2");
+    assert_eq!(evaluate(&input), None);
+}
+
+#[test]
+#[ignore]
+fn test_zero_operands_returns_none() {
+    let input = calculator_input("+");
+    assert_eq!(evaluate(&input), None);
+}
+
+#[test]
+#[ignore]
+fn test_intermediate_error_returns_none() {
+    let input = calculator_input("+ 2 2 *");
+    assert_eq!(evaluate(&input), None);
+}
